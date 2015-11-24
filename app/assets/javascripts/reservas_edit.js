206fefd4f1f0b42e18e3c9a1683f6fdf53d3beaf
Flavio Kon <flaviokr@gmail.com>
2015-11-24 17:46:13 +0000

    Finish edit reserva

--- app/assets/javascripts/reservas_edit.js
+++ app/assets/javascripts/reservas_edit.js
@@ -1,21 +1,23 @@
 $(function() {
     $('.agenda_livre').click( function() {
-        clicou = true;
+        var clicou = true;
     	$('.agenda_livre').css('background-color', '#fff'); //Altera tudo pro padrão normal
     	document.getElementById('sepultamento').value = $(this).attr("data-horario");
         document.getElementById('sala_id').value = $(this).attr("data-sala");
         
         //Guarda a sala
-        sala = $(this).attr("data-sala");
+        var sala = $(this).attr("data-sala");
 
         //Pinta o que foi clicado
         $(this).css('background-color', 'yellow'); 	
 
         //Pinta todos os outros
-        $(this).prevAll('.agenda_livre').each(function(){
-        	if($(this).attr("data-sala") == sala){
-        		$(this).css('background-color', 'yellow'); 	
-        	}        	
+        $(this).prevAll('div').each(function(){
+        	if($(this).attr("data-sala") == sala && $(this).hasClass('agenda_livre')){
+        		$(this).css('background-color', 'yellow');
+        	} else {
+        	    return false;
+        	}
         });       
     });
 });
