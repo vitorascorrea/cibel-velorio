bbe3c1ee5dd3983b2e790ee40ec8072bc5b1994f
Flavio Kon Rosenfeld <flaviokr@gmail.com>
2016-05-23 14:19:44 -0300

    controle de acesso

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include ApplicationHelper
-  
+
   before_filter :user_logado?
-  
+
   private
-  
+
   def user_logado?
     gon.nada = nil
-    if !logged_in? && params[:controller] != 'sessions'  
+    if !logged_in? && params[:controller] != 'sessions'
       redirect_to login_path
     end
+    if current_funcionario.class == Velorista && params[:controller] != 'home'
+      redirect_to home_path
+    end
   end
-  
+
 end
